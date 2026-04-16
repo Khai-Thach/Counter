3cbef59da28fadbfe5d8d5a219e75d3e596b1625
Khai Thach <tuq43307@temple.edu>
2026-04-16 12:08:03 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/counter/MainActivity.kt
@@ -0,0 +1,76 @@
+package edu.temple.counter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import edu.temple.counter.ui.theme.CounterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CounterTheme {
+            }
+        }
+    }
+}
+
+@Composable
+fun Counter(modifier: Modifier = Modifier) {
+
+    var counterVal: Int by remember { mutableStateOf(0) }
+
+    Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(onClick = {
+            counterVal++
+        }
+        ) {
+            Text("Up")
+        }
+
+        Text(
+            text = counterVal.toString(),
+            fontSize = 72.sp,
+            modifier = modifier
+        )
+
+        Button(onClick = {
+            if (counterVal > 0) {
+                counterVal--
+            }
+        }
+        ) {
+            Text("Down")
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    CounterTheme {
+        Counter()
+    }
+}
